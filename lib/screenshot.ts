3094ae761f5cbb7826e46648ad0d572e06e0d85b
Michelle <mliuu3259@gmail.com>
2021-02-14 21:00:02 -0500

    image download settings

--- lib/screenshot.ts
+++ lib/screenshot.ts
@@ -20,24 +20,24 @@ import puppeteer from 'puppeteer-core';
 export default async function screenshot(url: string) {
   const options = process.env.AWS_REGION
     ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-      }
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless
+    }
     : {
-        args: [],
-        executablePath:
-          process.platform === 'win32'
-            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-            : process.platform === 'linux'
+      args: [],
+      executablePath:
+        process.platform === 'win32'
+          ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+          : process.platform === 'linux'
             ? '/usr/bin/google-chrome'
             : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-      };
+    };
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.setViewport({ width: 2000, height: 1000 });
   //console.log(url);
-  await page.goto(url);
-  // await page.goto(url, { waitUntil: 'networkidle0' });
+  // await page.goto(url);
+  await page.goto(url, { waitUntil: 'networkidle2' });
   return await page.screenshot({ type: 'png' });
 }
