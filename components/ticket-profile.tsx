2821a27ea525499328e2b69ba4e9f888f4be383a
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-03-10 19:37:15 -0500

    get ticket 99% there

--- components/ticket-profile.tsx
+++ components/ticket-profile.tsx
@@ -14,44 +14,44 @@
  * limitations under the License.
  */
 
-import { TicketGenerationState } from '@lib/constants';
+import { useRef } from 'react';
 import cn from 'classnames';
+import { TicketGenerationState } from '@lib/constants';
 import styles from './ticket-profile.module.css';
 
 type Props = {
   name?: string;
-  username?: string;
+  ticketNumber?: number;
   size?: number;
   ticketGenerationState: TicketGenerationState;
 };
 
-export default function TicketProfile({ name, username, size = 1, ticketGenerationState }: Props) {
+export default function TicketProfile({ name, ticketNumber, ticketGenerationState }: Props) {
+  const imageIndex = useRef(Math.floor(Math.random() * 6) + 1);
+
   return (
-    <div className={styles.profile}>
-      <div className={styles.text}>
-        <p
-          className={cn(styles.name, {
-            [styles['name-blank']]: !username
-          })}
-        >
-          <span
-            className={cn(styles.skeleton, styles.wrapper, {
-              [styles.show]: ticketGenerationState === 'loading'
-            })}
-          >
-            {name || username || 'Your Name'}
-          </span>
+    <section className={styles.profile}>
+      <img className={styles.image} src={`/tickets/ticket-${imageIndex.current}.jpg`} width="2976" height="1674" />
+      <img className={styles.logo} src="/logo.svg" alt="TEDxCMU Logo" width="2976" height="1674" />
+      <div className={styles.content}>
+        <div>
+          <h3 className={styles.heading}>CATALYST CONFERENCE</h3>
+          <p className={styles.subheading}>ONLINE EXPERIENCE</p>
+        </div>
+        <p className={styles.name}>
+          {name || 'Your Name'}
         </p>
-        <p className={styles.username}>
-          <span
-            className={cn(styles.skeleton, styles.wrapper, {
-              [styles.show]: ticketGenerationState === 'loading'
-            })}
-          >
-            {username || <>username</>}
-          </span>
-        </p>
+        <div className={styles.details}>
+          <div className={styles.item}>
+            <p className={styles.title}>Date:</p>
+            <p className={styles.body}>April 4, 2021</p>
+          </div>
+          <div className={styles.item}>
+            <p className={styles.title}>Time:</p>
+            <p className={styles.body}>10:00AM EST</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
